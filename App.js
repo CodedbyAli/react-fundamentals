05fb32f9361c0a460b74f9aee308f7227cc16cfb
Ali Hamza <quotedbyali@gmail.com>
2024-09-17 09:49:59 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+const heading = React.createElement(
+    'h1',
+    {
+        id: "title",
+        style: {
+            color: 'blue',
+        }
+    },
+    'Ali Hamza'
+   )
+
+    const heading2 = React.createElement(
+        'h2',
+        {
+            id: "title"
+        },
+        'Its Pleasure to Meet You'
+    )
+
+    const container = React.createElement(
+        'div',
+        {
+            id: "container"
+        },
+        [heading,heading2]
+    )
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(container);
