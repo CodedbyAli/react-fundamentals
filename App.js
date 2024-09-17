a7b8a534e06a1fcf0a385a74f94bb83648326517
Ali Hamza <quotedbyali@gmail.com>
2024-09-17 14:00:06 +0500

    Chapter 3: Laying the foundations

--- App.js
+++ App.js
@@ -2,33 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const heading = React.createElement(
-    'h1',
-    {
-        id: "title",
-        style: {
-            color: 'blue',
-        }
-    },
-    'Ali Hamza'
-   )
+// Different ways to create a Functional Component
+    const HeadingComponent = () => <h1 className='FirstName'>My First name is Ali</h1>
 
-    const heading2 = React.createElement(
-        'h2',
-        {
-            id: "title"
-        },
-        'Its Pleasure to Meet You'
+    const NameComponent = () => {return <h2 className='LastName'>My Last name is Hamza</h2>}
+
+    const DesignationComponent = () => (
+        <h3>I am a Software Engineer and I have studied {degree}</h3>
     )
 
-    const container = React.createElement(
-        'div',
-        {
-            id: "container"
-        },
-        [heading,heading2]
+    const degree = <span>Bachelor's in Computer Sciences</span>
+
+    const Container = () => (
+        <>
+        {/* Three Ways to call a Functional Component */}
+            <HeadingComponent />
+            <NameComponent></NameComponent>
+            {DesignationComponent()}
+        </>
     )
+
 
     const root = ReactDOM.createRoot(document.getElementById('root'));
 
-    root.render(container);
+    root.render(<Container />);
